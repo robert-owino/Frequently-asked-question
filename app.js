a84a79a9c33409d134e15dbe30ab2a5ffda0d8dd
robert owino <97714329+robert-owino@users.noreply.github.com>
2023-03-13 19:11:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 //using selectors inside the element
 
-// const questions = document.querySelectorAll('.question')
+const questions = document.querySelectorAll('.question')
 
-// questions.forEach(function(question){
-// const btn = question.querySelector('.question-btn')
-// btn.addEventListener('click', function(){
-// questions.forEach(function(item){
-// if(item !== question){
-//     item.classList.remove('show-text')
-// }
-// })
+questions.forEach(function(question){
+const btn = question.querySelector('.question-btn')
+btn.addEventListener('click', function(){
+questions.forEach(function(item){
+if(item !== question){
+    item.classList.remove('show-text')
+}
+})
 
 
-//     question.classList.toggle('show-text')
-// })
-// })
+    question.classList.toggle('show-text')
+})
+ })
 
 
 
@@ -25,12 +25,12 @@
 
 //transvering the dom
 
-const btns = document.querySelectorAll('.question-btn')
+// const btns = document.querySelectorAll('.question-btn')
 
-btns.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-const style = e.currentTarget.parentElement.parentElement
-style.classList.toggle('show-text')
-    })
-})
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+// const style = e.currentTarget.parentElement.parentElement
+// style.classList.toggle('show-text')
+//     })
+// })
 
